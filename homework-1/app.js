78b890f62697dc229ce8a8ab5face8a52fbdd200
Medvedeva <medvedeva.maria6991@gmail.com>
2018-12-17 01:30:00 +0300

    add homework 2

--- homework-1/app.js
+++ homework-1/app.js
@@ -1,5 +1,6 @@
 import config from './config/config.json';
 import { User, Product } from './models';
+import { DirWatcher, Importer } from './modules';
 console.log(config.name);
 
 let user = new User('User module');
@@ -7,3 +8,7 @@ user.logModuleName();
 
 let product = new Product('Product module');
 product.logModuleName();
+
+let dirwatcher = new DirWatcher();
+dirwatcher.watch('./data', 1000);
+let importer = new Importer(dirwatcher);
