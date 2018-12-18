a4e006dd23a0a32f3751cb944af1b09c501f80e8
Maryia Miadzvedzeva <maryia_miadzvedzeva@epam.com>
2018-12-18 10:34:12 +0300

    add corrections

--- homework-1/app.js
+++ homework-1/app.js
@@ -9,6 +9,6 @@ user.logModuleName();
 let product = new Product('Product module');
 product.logModuleName();
 
-let dirwatcher = new DirWatcher();
+const dirwatcher = new DirWatcher();
 dirwatcher.watch('./data', 1000);
-let importer = new Importer(dirwatcher);
+const importer = new Importer(dirwatcher);
