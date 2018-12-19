41cee1f018bbbde53f03862c9dd8e74e30edbfe0
Medvedeva <medvedeva.maria6991@gmail.com>
2018-12-19 03:09:34 +0300

    add fixed watcher

--- homework-1/app.js
+++ homework-1/app.js
@@ -1,5 +1,8 @@
+import path from 'path';
 import config from './config/config.json';
 import { User, Product } from './models';
+import { DirWatcher, Importer } from './modules';
+import fs from 'fs-extra';
 console.log(config.name);
 
 let user = new User('User module');
@@ -7,3 +10,11 @@ user.logModuleName();
 
 let product = new Product('Product module');
 product.logModuleName();
+
+const dirwatcher = new DirWatcher();
+dirwatcher.watch('./data', 1000);
+const importer = new Importer(dirwatcher);
+
+setTimeout(() => fs.move('./test_data/fruits.csv', './data/fruits.csv'), 3000);
+setTimeout(() => fs.move('./test_data/product.csv', './data/product.csv'), 6000);
+setTimeout(() => fs.move('./test_data/user.csv', './data/user.csv'), 9000);
---
+++ homework-1/modules/dirwatcher.js
@@ -0,0 +1,30 @@
+import { readdir } from 'fs';
+import { EventEmitter } from 'events';
+
+export default class DirWatcher extends EventEmitter {
+  constructor() {
+    super();
+    this.timer = null;
+    this.dirs = [];
+  }
+
+  watch(path, delay) {
+    this.timer = setInterval(() => {
+      readdir(path, (error, files) => {
+        if (error) {
+          console.log(error);
+        }
+
+        if (!this.isEqual(this.dirs, files)) {
+          this.dirs = files;
+          console.log('changed');
+          this.emit('changed', files);    
+        }
+      });
+    }, delay);
+  }
+
+  isEqual(prevDirs, dirs) {
+    return prevDirs.length === dirs.length && prevDirs.filter(prevDir => !dirs.includes(prevDir)).length === 0;
+  }
+}
